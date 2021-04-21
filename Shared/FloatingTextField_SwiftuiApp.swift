d075d26a65c04f3465381c9f84fb3af35ff84bf9
nitanta.adhikari <nitanta.adhikari@ebpearsl.com>
2021-04-21 17:18:57 +0545

    Initial Commit

---
+++ Shared/FloatingTextField_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTextField_SwiftuiApp.swift
+//  Shared
+//
+//  Created by ebpearls on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTextField_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
